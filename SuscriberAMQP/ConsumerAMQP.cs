94284cde69ad7f65e15125e02642ca4ae5cd759d
Benigno Eduardo Santos Fernández <benignosantos91@gmail.com>
2024-02-16 17:14:33 -0500

    Nuevo suscriptor a que implemeta el protocolo AMQP
    -Agregado un suscriptor para brocker con el protocolo AMQP 1.0
    -Corrección de bugs y limpieza de código en otros suscriptores

--- SuscriberAMQP/ConsumerAMQP.cs
+++ SuscriberAMQP/ConsumerAMQP.cs
@@ -4,17 +4,16 @@ using InterfaceLibraryLogger;
 using InterfaceLibrarySubscriber;
 using Definitions;
 using NLog;
-using System.Text;
-using System.Security.Cryptography;
-using MQTTnet;
-using MQTTnet.Client;
-using MQTTnet.Extensions.ManagedClient;
+using Amqp;
+using Amqp.Framing;
 using System;
-using System.Reflection;
+using System.Collections.Generic;
+using Amqp.Listener;
+using System.Net;
+using Amqp.Types;
 
-namespace SuscriberMQTT
+namespace SuscriberAMQP
 {
-
     public class DestinationStruct
     {
         public string recipient = null;
@@ -26,20 +25,24 @@ namespace SuscriberMQTT
             this.deserializer = deserializer;
         }
     }
-    public class ConsumerMQTT : ISubscriber
-    {
 
+    public class ConsumerAMQP : ISubscriber
+    {
         private IConfigurator _configurator;
         private Logger _logger;
         private string _id;
         private Dictionary<string, DestinationStruct> _dicDestination = new Dictionary<string, DestinationStruct>();
         private bool bLoop = true;
 
-        private string sProtocolVersion = "3.1.1";
-        private string sHost;
         private string sPort;
+        private string sHost;
+        private string sessionId;
 
-        IManagedMqttClient mqttClient = new MqttFactory().CreateManagedMqttClient();
+        Connection host;
+        Session session;
+        List<ReceiverLink> lReceivers;
+        string sGuid = Guid.NewGuid().ToString();
+
 
         public void addDeserializer(string id, IDeserializer deserializer)
         {
@@ -85,15 +88,16 @@ namespace SuscriberMQTT
             try
             {
                 _logger.Trace("Inicio");
-                
+
                 bLoop = false;
 
-                var toDisconnect = mqttClient.StopAsync();
-                if (!toDisconnect.Wait(TimeSpan.FromSeconds(30)))   //30 segundos para suscribirse al topico en el broker
+                foreach (ReceiverLink item in lReceivers)
                 {
-                    throw new Exception($"30 segundos de tiempo de espera para la desconexion broker 'mqtt://{sHost}:{sPort}/' agotados");
+                    item.Close();
                 }
-                _logger.Trace("Fin");
+                session.Close();
+                host.Close();
+                lReceivers.Clear();
             }
             catch (Exception ex)
             {
@@ -108,7 +112,7 @@ namespace SuscriberMQTT
             {
                 _configurator = configurator;
                 _id = id;
-                _logger = (Logger)logger.init("SuscriberMQTT");
+                _logger = (Logger)logger.init("SuscriberAMQP");
                 object value;
                 if (!configurator.getMap(Config.Subscriptors, _id).TryGetValue(Config.Host, out value))
                 {
@@ -124,50 +128,13 @@ namespace SuscriberMQTT
                 }
                 sPort = value.ToString();
 
-                if (_configurator.getMap(Config.Subscriptors, _id).TryGetValue("version", out value))
+                if (!_configurator.getMap(Config.Subscriptors, _id).TryGetValue("sessionId", out value))
                 {
-                    sProtocolVersion = value.ToString();
+                    _logger.Error($"No se encontro el parametro 'sessionId' en el suscriptor de id '{_id}'");
+                    throw new Exception($"No se encontro el parametro 'sessionId' en el suscriptor de id '{_id}'");
                 }
-
-                // Create client options object
-                MqttClientOptionsBuilder builder;
-                if (sProtocolVersion == "5.0")  //MQTTv5.0
-                {
-                    builder = new MqttClientOptionsBuilder()
-                         .WithClientId(Guid.NewGuid().ToString())
-                         .WithTcpServer(sHost, Convert.ToInt32(sPort))
-                         .WithProtocolVersion(MQTTnet.Formatter.MqttProtocolVersion.V500);
-                }
-                else if (sProtocolVersion == "3.1.0")   //MQTTv3.1.0
-                {
-                    builder = new MqttClientOptionsBuilder()
-                        .WithClientId(Guid.NewGuid().ToString())
-                        .WithTcpServer(sHost, Convert.ToInt32(sPort))
-                        .WithProtocolVersion(MQTTnet.Formatter.MqttProtocolVersion.V310)
-                        .WithCleanSession();
-                }
-                else    //MQTTv3.1.1
-                {
-                    builder = new MqttClientOptionsBuilder()
-                        .WithClientId(Guid.NewGuid().ToString())
-                        .WithTcpServer(sHost, Convert.ToInt32(sPort))
-                        .WithProtocolVersion(MQTTnet.Formatter.MqttProtocolVersion.V311)
-                        .WithCleanSession();
-                }
-
-                ManagedMqttClientOptions options = new ManagedMqttClientOptionsBuilder()
-                       .WithAutoReconnectDelay(TimeSpan.FromSeconds(5))         //Autoreconectar con el servidor ante fallas a los 5 segundos
-                       .WithClientOptions(builder.Build())
-                       .Build();
-
-
-                var connectResult = mqttClient.StartAsync(options);
-                if (!connectResult.Wait(TimeSpan.FromSeconds(30)))   //30 segundos para conectarse al servidor
-                {
-                    _logger.Error($"30 segundos de tiempo de espera para conexion con el broker 'mqtt://{sHost}:{sPort}/' agotados");
-                    throw new Exception($"30 segundos de tiempo de espera para conexion con el broker 'mqtt://{sHost}:{sPort}/' agotados");
-                }
-
+                sessionId = value.ToString();
+                
                 object tempValue;
                 if (!configurator.getMap(Config.Subscriptors, _id).TryGetValue("mssgBoxes", out tempValue))
                 {
@@ -181,6 +148,7 @@ namespace SuscriberMQTT
                     _logger.Error($"No se encontro ningun parametro en 'mssgBoxes' en el suscriptor de id '{_id}'");
                     throw new Exception($"No se encontro el parametro en 'mssgBoxes' en el suscriptor de id '{_id}'");
                 }
+
                 for (int index = 0; index < listMssgBoxes.Count; index++)
                 {
                     Dictionary<string, string> tempMssgBox = System.Text.Json.JsonSerializer.Deserialize<Dictionary<string, string>>(listMssgBoxes[index].ToString());
@@ -197,6 +165,13 @@ namespace SuscriberMQTT
                     }
                     this._dicDestination.Add(tempDetination.recipient, tempDetination);
                 }
+
+                // Dirección del servidor AMQP
+                Address address = new Address("amqp://" + sHost + ":" + sPort);
+                // Crear un host y registrar un transportador de transporte personalizado
+                host = new Connection(address);
+                session = new Session(host);
+
             }
             catch (Exception ex)
             {
@@ -211,27 +186,26 @@ namespace SuscriberMQTT
             {
                 _logger.Trace("Inicio");
 
-                mqttClient.ApplicationMessageReceivedAsync += e =>
+                foreach (ReceiverLink receiver in lReceivers)
                 {
-                    DestinationStruct currentDeserializer = new DestinationStruct();
-
-                    string sTopic = e.ApplicationMessage.Topic;
-
-                    if (!this._dicDestination.TryGetValue(sTopic, out currentDeserializer))
+                    receiver.Start(1, async (link, message) =>
                     {
-                        _logger.Error("No se encontro registrada en el modulo la tarea '{0}'", sTopic);
-                        throw new Exception($"No se encontro registrada en el modulo la tarea '{sTopic}'");
-                    }
-                    Dictionary<string, object> metadata = new Dictionary<string, object>();
-                    metadata.Add("recipientName", (object)sTopic);
-                    var data = System.Text.Encoding.UTF8.GetString(e.ApplicationMessage.PayloadSegment);
-                    var bytes = System.Text.Encoding.UTF8.GetBytes(data);
+                        DestinationStruct currentDeserializer = new DestinationStruct();
 
-                    currentDeserializer.deserializer.deserialize(bytes, metadata);
+                        string sQueue = link.Name;
 
-                    return Task.CompletedTask;
-                };
+                        if (!this._dicDestination.TryGetValue(sQueue, out currentDeserializer))
+                        {
+                            _logger.Error("No se encontro registrada en el modulo la tarea '{0}'", sQueue);
+                            throw new Exception($"No se encontro registrada en el modulo la tarea '{sQueue}'");
+                        }
+                        Dictionary<string, object> metadata = new Dictionary<string, object>();
+                        metadata.Add("recipientName", (object)sQueue);
+                        var bytes = System.Text.Encoding.UTF8.GetBytes(message.Body.ToString());
 
+                        currentDeserializer.deserializer.deserialize(bytes, metadata);
+                    });
+                }
                 _logger.Trace("Fin");
                 while (bLoop)
                 {
@@ -260,11 +234,8 @@ namespace SuscriberMQTT
                         recipient = recipient + "_" + subRecipent;
                     }
 
-                    var toSuscribe = mqttClient.SubscribeAsync(recipient);
-                    if (!toSuscribe.Wait(TimeSpan.FromSeconds(30)))   //30 segundos para suscribirse al topico en el broker
-                    {
-                        throw new Exception($"30 segundos de tiempo de espera para suscribirse al 'topico{recipient}' en el broker 'mqtt://{sHost}:{sPort}/' agotados");
-                    }
+                    var receiver = new ReceiverLink(session, sessionId, recipient);
+                    lReceivers.Add(receiver);
                 }
                 _logger.Trace("Fin");
             }
@@ -274,6 +245,5 @@ namespace SuscriberMQTT
                 throw ex;
             }
         }
-        
     }
 }
